3fa7145d1c330548a24c2610287501a977e69154
Danish <danishsidd524@gmail.com>
2024-10-15 00:29:39 +0500

    init: database using prisma

---
+++ apps/server/prisma/migrations/20241014192446_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Messages" (
+    "id" UUID NOT NULL,
+    "message" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Messages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Messages_id_key" ON "Messages"("id");
