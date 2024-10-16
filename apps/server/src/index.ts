416e61912bca33553eee16a5b7c3d3aa4ac5042a
Danish <danishsidd524@gmail.com>
2024-10-17 00:33:07 +0500

    add: kafka consumer and database entries

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,10 @@
 import http from "http";
 import SocketService from "./services/socket";
 import { getRedisClient } from "./services/redis";
+import { startMessageConsumer } from "./services/kafka";
 
 async function init() {
+  startMessageConsumer();
   const { pub, sub } = await getRedisClient();
   const socketService = new SocketService(pub, sub);
   const server = http.createServer();
--- apps/server/src/services/kafka.ts
+++ apps/server/src/services/kafka.ts
@@ -1,6 +1,7 @@
 import { Kafka, Producer } from "kafkajs";
 import fs from "fs";
 import path from "path";
+import prisma from "./prisma";
 
 const kafka = new Kafka({
   brokers: [process.env.KAFKA_BROKER as string],
@@ -31,6 +32,37 @@ export const produceMessage = async (message: string) => {
     topic: "Messages",
   });
   return true;
+};
+
+export const createConsumer = async (topic: string) => {
+  const consumer = kafka.consumer({ groupId: "test-group" });
+  await consumer.connect();
+  await consumer.subscribe({ topic, fromBeginning: true });
+  return consumer;
+};
+
+export const startMessageConsumer = async () => {
+  const consumer = await createConsumer("Messages");
+  await consumer.run({
+    autoCommit: true,
+    eachMessage: async ({ topic, partition, message, pause }) => {
+      console.log("Received message", message);
+      if (!message.value) return;
+      try {
+        await prisma.messages.create({
+          data: {
+            message: message.value.toString(),
+          },
+        });
+      } catch (error) {
+        console.log("Something went wrong", error);
+        pause();
+        setTimeout(() => {
+          consumer.resume([{ topic: "Messages" }]);
+        }, 60 * 1000);
+      }
+    },
+  });
 };
 
 export default kafka;
